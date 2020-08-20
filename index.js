c8bffadce11e76d148bb3f91edaf8503bc2e141b
Name boy <53903319+namekinggamerboy@users.noreply.github.com>
2020-08-20 07:37:03 +0530

    Update index.js
    Done

--- index.js
+++ index.js
@@ -2141,25 +2141,23 @@ getUserMention(mention) {
 	}
 },
 getEmojiMention(mention) {
+
 	if (!mention) return;
-if (mention.startsWith('<a') && mention.endsWith('>')) {
-		mention = mention.slice(2, -1);
 
-		/* if (mention.startsWith('!')) {
-			mention = mention.slice(1);
-		} */
-
-		return client.emojis.get(mention);
-	} else 
 	if (mention.startsWith('<') && mention.endsWith('>')) {
+
 		mention = mention.slice(2, -1);
 
-		/* if (mention.startsWith('!')) {
+		 if (mention.startsWith('a')) {
+
 			mention = mention.slice(1);
-		} */
+
+		} 
 
 		return client.emojis.get(mention);
+
 	}
+
 },
 
 colorRole(name, color){
