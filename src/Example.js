9ebc5f7adb78eb83f9f8aa9e2f187365db196a93
Name boy <53903319+namekinggamerboy@users.noreply.github.com>
2020-10-02 13:08:46 +0530

    Update Example.js

--- src/Example.js
+++ src/Example.js
@@ -1,8 +1,15 @@
 /**
  * Represents a bot Function.
  */
+class Function extends EventEmitter {
+    /**
+     * @param {Discord.Snowflake} guildID ID of the guild this queue is for.
+     */
+    constructor (guildID) {
 
 /**
          * ID of the guild this queue is for.
          * @type {Discord.Snowflake}
          */
+}
+}
